f23edf61427a6dd099e772d3ad4ac4d122d8c994
ayoubbenjaa <ayoubbenjaa642@gmail.com>
2023-11-07 20:12:33 +0100

    Login page from ayoub

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Landing from "./Pages/Home/Landing";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <div>
       <Landing></Landing>
+      <Login></Login>
     </div>
   );
 }
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <div>   
+            
+            <h1 className='text-5xl text-center '>This is the login page from Benjaa</h1>
+    </div>
+  )
+} 
+
+export default Login
