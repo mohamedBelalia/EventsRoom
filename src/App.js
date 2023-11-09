9ddfed0c0fef7e60da12b4d30f2c237eebb9dd88
Mohamed <mohamedbelaliap@gmail.com>
2023-11-09 11:49:38 +0100

    START

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Landing from "./Pages/Home/Landing";
-import Login from "./Pages/Login/Login";
-
 function App() {
   return (
     <div>
-      <Landing></Landing>
-      <Login></Login>
+
     </div>
   );
 }
